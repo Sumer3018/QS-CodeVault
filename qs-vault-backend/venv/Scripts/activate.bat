637a6dc3475599d9ee2936b9b1d1f4f8cc1da941
Sumer <singhsumer3018@gmail.com>
2026-04-20 15:29:00 +0530

    Project Completed and Dataset Uploaded - needs some more touchups

--- qs-vault-backend/venv/Scripts/activate.bat
+++ qs-vault-backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Sem 8\Project\QS-CodeVault\qs-vault-backend\venv
+set VIRTUAL_ENV=D:\Sem8\Project\QS-CodeVault\qs-vault-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
